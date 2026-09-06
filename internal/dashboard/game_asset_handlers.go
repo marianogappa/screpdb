8b7d32f9365aa9d118cdf82905b90d394db7d712
Mariano Gappa <spinetta@gmail.com>
2026-09-06 15:27:50 +0400

    chore: gofmt the wasm build-tag files

--- internal/dashboard/game_asset_handlers.go
+++ internal/dashboard/game_asset_handlers.go
@@ -1,4 +1,5 @@
 //go:build !js
+
 package dashboard
 
 import (
--- internal/selfupdate/selfupdate_js.go
+++ internal/selfupdate/selfupdate_js.go
@@ -10,8 +10,8 @@ import (
 var errNotAvailable = errors.New("self-update is not available in the browser preview")
 
 func CheckStatus(context.Context) (Status, error) { return Status{Tier: TierNone}, errNotAvailable }
-func Apply(context.Context) (string, error)        { return "", errNotAvailable }
-func CleanupOldBinary()                            {}
-func IsRestart() bool                              { return false }
-func RestartEnvKV() string                         { return "" }
-func Restart() error                               { return errNotAvailable }
+func Apply(context.Context) (string, error)       { return "", errNotAvailable }
+func CleanupOldBinary()                           {}
+func IsRestart() bool                             { return false }
+func RestartEnvKV() string                        { return "" }
+func Restart() error                              { return errNotAvailable }
